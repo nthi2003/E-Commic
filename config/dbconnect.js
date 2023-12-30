5d32ad642549e5fec0f9ad47182501cfdecbb73a
nthi2003 <quocthi.dev@gmail.com>
2023-12-30 15:13:09 +0700

    update 1

---
+++ config/dbconnect.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require('mongoose')
+
+const dbConnect = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL)
+        if (conn.connection.readyState === 1) console.log(`DB connection is successfully `)
+        else console.log(`DB connection`);
+
+     } catch(error){
+        console.log('DB connection is failed')
+        throw new Error(error)
+     }
+
+
+}
+module.exports = dbConnect
